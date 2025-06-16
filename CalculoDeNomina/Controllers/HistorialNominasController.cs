4e05cfade51127b2417c315cd011401f6601cd0d
Hiram Gonzalez <hiramgzzr@gmail.com>
2025-06-15 20:39:02 -0600

    Funcionamiento_del_correo_listo

--- CalculoDeNomina/Controllers/HistorialNominasController.cs
+++ CalculoDeNomina/Controllers/HistorialNominasController.cs
@@ -5,16 +5,26 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Net.Mime;
+using System.Security;
 using System.Text;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.WebPages;
+
 
 namespace CalculoDeNomina.Controllers
 {
     [Authorize]
     public class HistorialNominasController : Controller
     {
+        //public string fechaCalculo;
+        //public string status;
+        //public string cantidadFilas;
+        //public string tablaExcel;
+
         // GET: HistorialNominas
         public ActionResult HistorialNominas()
         {
@@ -34,6 +44,10 @@ namespace CalculoDeNomina.Controllers
                 if(registro == null) return HttpNotFound();
 
                 ViewBag.TablaExcel = GenerarHTMLArchivoExcel(registro.ArchivoNomina);
+                //this.tablaExcel = GenerarHTMLArchivoExcel(registro.ArchivoNomina);
+                //this.fechaCalculo = registro.FechaCalculo.ToString();
+                //this.status = registro.Estado ? "Procesado" : "En proceso";
+                //this.cantidadFilas = registro.FilasAfectadas.ToString();
                 ViewBag.MostrarModal = false;
 
                 return View(registro);
@@ -117,12 +131,77 @@ namespace CalculoDeNomina.Controllers
 
         // Metodo que se usa para el envio de correos
         [HttpPost]
-        public ActionResult EnviarCorreo()
+        [ValidateInput(false)]
+        public ActionResult EnviarCorreo(
+            string correoDestinatario, 
+            string correoCopia, 
+            string asunto, 
+            string fechaCalculo,
+            string status,
+            string cantidadFilas,
+            string tablaExcel,
+            string id,
+            string mensaje = ""
+            )
         {
+            try
+            {
+                MailMessage correo = new MailMessage();
+                correo.From = new MailAddress("hiramagzzrdz@yahoo.com.mx");
 
+                string[] destinatarios = SepararCadenaDeCorreos(correoDestinatario);
+                foreach (string destinatario in destinatarios)
+                {
+                    correo.To.Add(destinatario);
+                }
 
-            return Json(new { ok = false });
+                if(correoCopia != null || !correoCopia.IsEmpty())
+                {
+                    string[] copias = SepararCadenaDeCorreos(correoCopia);
+                    foreach (string cc in copias)
+                    {
+                        correo.CC.Add(cc);
+                    }
+                }
+
+                correo.Subject = asunto;
+                correo.Body = string.Format("<html>\r\n<body>\r\n<p>{0}</p>\r\n\r\n<h3>Datos de la nómina</h3>\r\n<table>\r\n<tr>\r\n<td>\r\n<p><b>Fecha de cálculo</b></p>\r\n</td>\r\n<td>\r\n <p><b>{1}</b></p>\r\n</td>\r\n</tr>\r\n<tr>\r\n <td>\r\n  <p><b>Status</b></p>\r\n</td>\r\n <td>\r\n<p><b>{2}</b></p>\r\n</td>\r\n</tr>\r\n  <tr>\r\n <td>\r\n <p><b>Cantidad de filas de la nómina</b></p>\r\n</td>\r\n<td>\r\n<p><b>{3}</b></p>\r\n </td>\r\n</tr>\r\n</table>\r\n\r\n<h3>Contenido de la nómina</h3>\r\n{4}\r\n </body>\r\n </html>",
+                    mensaje,
+                    fechaCalculo,
+                    status.Equals("1") ? "Procesado" : "En proceso",
+                    cantidadFilas,
+                    tablaExcel
+                );
+
+                correo.IsBodyHtml = true;
+
+                using (var context = new CalculosDeNominaEntities())
+                {
+                    var registro = context.CalculosDeNomina.FirstOrDefault(m => m.ID.ToString() == id);
+                    if (registro.ArchivoNomina != null)
+                    {
+                        MemoryStream ms = new MemoryStream(registro.ArchivoNomina);
+
+                        Attachment adjunto = new Attachment(ms, registro.NombreCalculo + ".xlsx", MediaTypeNames.Application.Octet);
+                        correo.Attachments.Add(adjunto);
+                    }
+                }
+
+                SmtpClient client = new SmtpClient();
+                client.Send(correo);
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error al enviar correo: " + e.Message);
+                Console.WriteLine("Detalles: " + e.InnerException.Message);
+                throw new Exception("Error al enviar correo: " + e.Message);
+            }
+
+            return RedirectToAction("DetalleNomina", new { id = int.Parse(id) });
         }
+
+
 
         // Metodo que se usa para generar el HTML para mostrar el contenido del archivo Excel
         private string GenerarHTMLArchivoExcel(byte[] archivo)
@@ -166,6 +245,19 @@ namespace CalculoDeNomina.Controllers
 
             return archivoBytes;
         }
+
+
+        private string[] SepararCadenaDeCorreos(string correos)
+        {
+            string[] arrayCorreos = correos.Split(',');
+            for(int i = 0; i < arrayCorreos.Length; i++)
+            {
+                arrayCorreos[i] = arrayCorreos[i].Trim();
+            }
+
+            return arrayCorreos;
+        }
+
 
         [HttpPost]
         public ActionResult MostrarModalCorreo()
